5cc011fac492163ec97751c8948830dbc2d88726
Vivek N <proviveknayan@gmail.com>
2026-02-25 14:30:44 +0530

    be dest  change

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: "https://apps.labour.gov.in/labourchowk/api/public/api/", // 🌐 your backend base URL
-    baseURL: "http://13.235.48.250/api/", // 🌐 DocumentRoot is /var/www/dlc-app/public — app served at server root
+    // baseURL: "http://13.235.48.250/api/", // 🌐 DocumentRoot is /var/www/dlc-app/public — app served at server root
+    baseURL: "https://dlc-app-api.indusaction.org/",
     timeout: 10000, // 10 sec timeout
 });
 
